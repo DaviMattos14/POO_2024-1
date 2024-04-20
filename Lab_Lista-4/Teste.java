13d48584ec3a264a28d099691141f954bee746e4
Davi Mattos <davimattos2012@gmail.com>
2024-04-20 20:53:46 -0300

    Laboratório e Lista 4

--- Lab_Lista-4/Teste.java
+++ Lab_Lista-4/Teste.java
@@ -4,14 +4,14 @@ public class Teste{
         Gerente jose = new Gerente("Jose", 2222, 10500.00, null);
         Funcionario eugenio = new Funcionario("Eugenio",9999,4500.00, jose);
         Funcionario carlos = new Funcionario("Carlos",1234,4500.00, jose);
-        Funcionario carlito = new Funcionario("Carlito",1234,4500.00, jose);
+        Funcionario carlito = new Funcionario("Carlito",1234,1412.00, jose);
         Trimestre t = Trimestre._3o;
 
         System.out.println(carlos.equals(carlito));
 
-        //System.out.println("\nBônus de " + jose.getNome() + " no " + t + " trimestre");
-        //System.out.println(jose.bonus(t));
-        //System.out.println("\nBônus de " + eugenio.getNome() + " no " + t + " trimestre");
-        //System.out.println(eugenio.bonus(t));
+        System.out.println("\nBônus de " + jose.getNome() + " no " + t + " trimestre\n");
+        System.out.println(jose.bonus(t));
+        System.out.println("\nBônus de " + eugenio.getNome() + " no " + t + " trimestre\n");
+        System.out.println(eugenio.bonus(t));
     }
 }
